700a944685d86f9f332e00c0514b5e8c700d1bb3
unknown <Administrador@UH-014-10.uni.lan>
2022-09-30 08:22:03 +0200

    actualizacion

---
+++ UD1/1-Introduction/src/CylinderComputation.java
@@ -0,0 +1,17 @@
+public class CylinderComputation { 
+    public static void main(String[] args) {
+       double radius, height, surfaceArea, baseArea, volume;
+       final double PI = 3.14159265 ;
+       radius = 1.2;
+       height = 2;
+       surfaceArea = 2 * PI * radius * radius; 
+       baseArea = PI * radius * radius;
+       volume = PI *  radius * radius   * height;
+       System.out.print("The surfaceArea is"); 
+       System.out.println(surfaceArea);
+       System.out.print("The baseArea is ");
+       System.out.println(baseArea);
+       System.out.print("The volume is ");
+       System.out.println(volume);
+    }
+ }
---
+++ UD1/1-Introduction/src/RectangleComputation.java
@@ -0,0 +1,15 @@
+public class RectangleComputation {  
+    public static void main(String[] args) { 
+           double length, width, area, perimeter;
+       length = 1.2;
+       width = 2;
+       area = length * width ;
+      perimeter= 2* length + 2* width ;
+       
+       // Print results
+       System.out.print("The area is "); 
+       System.out.println(area);         
+       System.out.print("The perimeter is ");
+       System.out.println(perimeter);
+    }
+ }
