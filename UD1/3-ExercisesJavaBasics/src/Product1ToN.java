7138331ecc9f49152c0ebb7da7f7e8dbc3309095
unknown <Administrador@UH-014-10.uni.lan>
2022-10-04 12:54:00 +0200

    m

--- UD1/3-ExercisesJavaBasics/src/Product1ToN.java
+++ UD1/3-ExercisesJavaBasics/src/Product1ToN.java
@@ -1,17 +1,17 @@
-public class Product1ToN {   // Save as "SumAverageRunningInt.java"
-public static void main(String[] args) {
-   final long LOWERBOUND = 1;
-   final long UPPERBOUND = 10;
-   long product  = 1;    // For accumulating odd numbers, init to 0
-   long number = LOWERBOUND;
-   if (number <= UPPERBOUND) {
-     product = product * number ;
-         
-      }
-      ++number;  // Next number
-   
-   // Print the result
-   System.out.println("The product of numbers from " + LOWERBOUND + " to " + UPPERBOUND + " is " + (product));
-  
-}
+public class Product1ToN { // Save as "SumAverageRunningInt.java"
+    public static void main(String[] args) {
+        final long LOWERBOUND = 1;
+        final long UPPERBOUND = 10;
+        long product = 1; // For accumulating odd numbers, init to 0
+        long number = LOWERBOUND;
+        while (number <= UPPERBOUND) {
+            product = product * number;
+
+        }
+        ++number; // Next number
+
+        // Print the result
+        System.out.println("The product of numbers from " + LOWERBOUND + " to " + UPPERBOUND + " is " + product);
+
+    }
 }
