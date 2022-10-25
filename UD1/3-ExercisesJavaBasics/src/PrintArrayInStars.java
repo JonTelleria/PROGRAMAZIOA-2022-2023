004c0149fa703718224ba1588a7b1683c05b0728
unknown <Administrador@UH-014-10.uni.lan>
2022-10-25 14:22:27 +0200

    2

---
+++ UD1/3-ExercisesJavaBasics/src/PrintArrayInStars.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class PrintArrayInStars {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        final int NUM_ITEMS;
+        int[] items; 
+        
+        //IT IS UP TO YOU TO INCLUDE SO MANY BLANK LINES (...ALTHOUGTH I THINK THEY ARE TOO MANY)
+  
+   
+
+        System.out.print("Enter the length of the arrays "); 
+        NUM_ITEMS = in.nextInt();                     
+
+
+
+        items = new int[NUM_ITEMS];
+  
+        if (items.length > 0) { //TAKE CARE OF THE INDENTATION. IT IS NOT OPTIONAL. YOU CAN USE THE "FORMAT DOCUMENT" COMMAND IN YOUR IDE.
+
+            System.out.print("Enter the value of all items (separated by space):");
+           
+           for (int i = 0; i < items.length; ++i) { 
+
+            items[i] = in.nextInt();                     
+
+           }
+        }else{
+            System.out.println("You are stupid you can't enter a negative length :( ");
+
+        }
+  
+        
+        for (int i = 0; i < items.length; ++i) {
+
+              System.out.print(i +1);
+              for(int x = 0; x < items[i]; x++){
+              System.out.print("*");
+              }
+              System.out.print("(" + items[i] + ")");
+              System.out.println();
+
+              
+        }
+
+
+
+            
+        in.close();
+    }
+
+}
---
+++ UD1/4-BesteAriketaBatzuk/src/arrayengaldetegia/Bikoitza2.java
@@ -0,0 +1,46 @@
+package arrayengaldetegia;
+
+import java.util.Scanner;
+
+public class Bikoitza2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] items;
+        items = new int[5];
+
+        System.out.print("Enter the value of 5 items (separated by space):");
+
+        for (int i = 0; i < items.length; ++i) {
+
+            items[i] = in.nextInt();
+
+        }
+
+        System.out.print("[");
+        for (int i = 0; i < items.length; ++i) {
+            if (i == 0) {
+
+                System.out.print(items[0]);
+            } else {
+
+                System.out.print(", " + items[i]);
+            }
+        }
+        System.out.println("]");
+
+        System.out.print("[");
+        for (int i = 0; i < items.length; ++i) {
+            if (i == 0) {
+
+                System.out.print(items[0] * 2);
+            } else {
+
+                System.out.print(", " + items[i] * 2);
+            }
+        }
+        System.out.println("]");
+
+        in.close();
+    }
+
+}
