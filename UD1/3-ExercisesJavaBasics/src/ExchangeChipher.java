3051391cb3aefe1d33787cf0d85a549a7af8a571
Jon Telleria <telleria.jon@uni.eus>
2022-11-03 21:41:06 +0100

    2

---
+++ UD1/3-ExercisesJavaBasics/src/ExchangeChipher.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ExchangeChipher {
+    public static void main(String[] args) {
+        String inStr;
+        int Strlen;
+        char momch;
+        int numb;
+
+        Scanner in = new Scanner(System.in);  
+        System.out.print("Enter a String: "); 
+        inStr = in.next();    
+        in.close();
+
+        Strlen = inStr.length();
+        inStr = inStr.toUpperCase();
+
+        for (int x =0; x < Strlen; x++){
+            momch = inStr.charAt(x);
+            
+            numb = momch - 65;
+
+            momch =(char)( 90 - numb);
+
+            System.out.print(momch);
+
+        }
+        
+    }
+}
---
+++ UD1/3-ExercisesJavaBasics/src/arrayToString.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class arrayToString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        final int NUM_ITEMS;
+        int[] items;
+
+        System.out.print("Enter the length of the arrays ");
+        NUM_ITEMS = in.nextInt();
+
+        items = new int[NUM_ITEMS];
+
+        if (items.length > 0) {
+
+            System.out.print("Enter the value of all items (separated by space):");
+
+            for (int i = 0; i < items.length; ++i) {
+
+                items[i] = in.nextInt();
+
+            }
+        } else {
+            System.out.println("Enter a positive length ");
+
+        }
+
+        in.close();
+
+        System.out.println(ArraytoString(items)); 
+    }
+
+    public static String ArraytoString(int[] items){
+        String list = "[";
+
+        for(int x = 0; x < items.length; x++){
+            if(x == items.length-1){
+                list += items[x];
+            }else{
+                    
+                list += items[x] +", ";
+            }
+        }
+            
+        list +=  ']';
+        return list;
+    }
+}
